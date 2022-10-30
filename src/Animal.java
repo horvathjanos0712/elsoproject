46f4256b2878688e82a38d50ee8f1942fcf62b81
horvathjanos0712 <horvathjanos0712@outlook.hu>
2022-10-31 00:10:09 +0100

    Poliformizmus

---
+++ src/Animal.java
@@ -0,0 +1,19 @@
+public class Animal {
+    String name;
+    int weight;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
--- src/ElsoProject.java
+++ src/ElsoProject.java
@@ -1,10 +1,15 @@
 public class ElsoProject {
 
     public static void main(String[] args) {
-        Human first = new Human();
+        Cat macska = new Cat();
+        Cat macska2 = new Cat();
+
+        //System.out.println(macska == macska2);
+
+       /* Human first = new Human();
         first.setName("Gyula");
         first.setAge(18);
-        System.out.println(first.getName() == null ? "üres" : "Nem üres, az értéke " + first.getName());
+        System.out.println(first.getName() == null ? "üres" : "Nem üres, az értéke " + first.getName());*/
 
 
     /*if (first.getName() == null) {
